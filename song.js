86fecae172d599f9fe21963dbe4c003b8f7dec30
Saša Mihaljenović <smihaljenovic@gmail.com>
2019-01-15 06:31:41 +0100

    Data Types homework

---
+++ song.js
@@ -0,0 +1,25 @@
+/*
+ * Keeping Up With the Javascripts
+ *
+ * Data Types homework
+ */
+var name = 'Bohemian Rhapsody';     // song name
+var releaseYear = 1975;     // released date
+var genre = 'Pop/Rock';
+var styles = ['Art Rock', 'Hard Rock', 'Prog-Rock'];
+var author = {
+    lyrics: 'Freddie Mercury',
+    composed: 'Freddie Mercury'
+};
+var isEvergreen = true;
+
+// song output
+console.log(name);
+console.log(releaseYear);
+console.log(genre);
+console.log(styles);
+console.log(styles[0]);
+console.log(author);
+console.log(author.lyrics);
+console.log(author.composed);
+console.log(isEvergreen);
